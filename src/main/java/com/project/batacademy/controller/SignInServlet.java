84a02f239c2a80a1cddf829887d4a6ab1b3bc4eb
Geeta Bhavi <geeta@Geetas-MacBook-Pro.local>
2016-08-02 19:55:20 -0700

    js changes

--- src/main/java/com/project/batacademy/controller/SignInServlet.java
+++ src/main/java/com/project/batacademy/controller/SignInServlet.java
@@ -12,30 +12,32 @@ import javax.servlet.http.HttpServletResponse;
  *
  * @author geeta
  */
-public class SignInServlet extends HttpServlet  {
+public class SignInServlet extends HttpServlet {
+
     private AuthenticateUser authUser = new AuthenticateUser();
-    private final String CONTENT_TYPE= "text/html";
+    private final String CONTENT_TYPE = "text/html";
 
     public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         PrintWriter out = response.getWriter();
         response.setContentType(CONTENT_TYPE);
-        
-        int userId = Integer.parseInt(request.getParameter("id"));
+        String id = request.getParameter("id");
         String pwd = request.getParameter("password");
-        
-        System.out.println(userId+","+pwd);
-        if(authUser.checkIfUserExists().equalsIgnoreCase("student")) {
-            //request.getRequestDispatcher("/UserDetailsStudent.jsp").forward(request, response);
-            out.write("student");
-            
-        } else if(authUser.checkIfUserExists().equalsIgnoreCase("faculty")) {
-            //request.getRequestDispatcher("/UserDetailsFaculty.jsp").forward(request, response);
-            out.write("faculty");
-        } else {
-            out.write("error");
-            out.close();
+        if (id.length() != 0 && pwd.length() != 0) {
+            int userId = Integer.parseInt(id);
+
+            System.out.println(userId + "," + pwd);
+            if (authUser.checkIfUserExists().equalsIgnoreCase("student")) {
+                //request.getRequestDispatcher("/UserDetailsStudent.jsp").forward(request, response);
+                out.write("student");
+
+            } else if (authUser.checkIfUserExists().equalsIgnoreCase("faculty")) {
+                //request.getRequestDispatcher("/UserDetailsFaculty.jsp").forward(request, response);
+                out.write("faculty");
+            } else {
+                out.write("error");
+                out.close();
+            }
         }
-        
 
     }
 
